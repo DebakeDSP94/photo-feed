89b96fef8b63bd23663b63f3c00f0e1be512388b
stuart wilson <stuartwilson94@gmail.com>
2023-01-04 13:44:16 -0600

    added search by tags, and did some styling

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,6 +5,10 @@ class StaticPagesController < ApplicationController
     @flickr = Flickr.new
     if params[:user_id].present?
       @photos = @flickr.people.getPhotos user_id: params[:user_id]
+    elsif params[:tags].present?
+      @photos = @flickr.photos.search tags: params[:tags], safesearch: 1
+    else
+      @photos = @flickr.photos.getRecent
     end
   end
 end
